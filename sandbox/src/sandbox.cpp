ca318dfbfa5ff2771ffad235be55c95dae1ef2a3
geohan98 <43745686+geohan98@users.noreply.github.com>
2020-05-10 16:51:56 +0100

    Removed Some Unused Code

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -11,20 +11,12 @@
 Sandbox::Sandbox(char* _name) : Engine::Application(_name)
 {
 	LOG_INFO("[{0}][START]", _name);
-	//m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::GameLayer()));
-	//m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::MeshLayer()));
-	//m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::uiButtons()));
+	m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::MeshLayer()));
+	m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::uiButtons()));
 
-	//Engine::ImGUILayer* imGuiInstance = new Engine::ImGUILayer();
-	//m_layerStack->push(std::shared_ptr<Engine::Layer>(imGuiInstance));
-	m_layerStack->push(std::shared_ptr<Engine::Layer>(new Engine::Layer2D));
-
-	//imGuiInstance->createButton("1", { 100, 50 }, { 100, 100 }, "pos(100,100) button", ImVec4(0.8f, 0.4f, 1.0f, 1.0f), -1, [this] { buttonClickedExample(); });
-
-	std::unordered_map < std::string, unsigned int> fonts;
-	fonts.emplace(std::pair<std::string, unsigned int>("assets/fonts/Roboto-Medium.ttf", 48));
-
-	m_resourceManager->populateCharacters(fonts);
+	Engine::ImGUILayer* imGuiInstance = new Engine::ImGUILayer();
+	m_layerStack->push(std::shared_ptr<Engine::Layer>(imGuiInstance));
+	imGuiInstance->createButton("1", { 100, 50 }, { 100, 100 }, "pos(100,100) button", ImVec4(0.8f, 0.4f, 1.0f, 1.0f), -1, [this] { buttonClickedExample(); });
 }
 
 Sandbox::~Sandbox()
