9863ca7833166650b0f4322984815487b715539f
= <spencerjdeane@gmail.com>
2020-05-20 15:57:23 +0100

    dOxygen Commenting

--- sandbox/include/imguiLayer.h
+++ sandbox/include/imguiLayer.h
@@ -6,40 +6,25 @@
 #include "Headers/renderer/texture.h"
 
 namespace Engine {
+
+	/**
+	 *  ImGUI Layer, Handles creation, loading and event handling of ImGUI buttons
+	 */
 	class ImGUILayer : public Layer {
 	public:
-		ImGUILayer();
-
-		std::shared_ptr<Renderer::Texture> texturePtr;
+		ImGUILayer(); ///< Constructor
 
 		void onAttach() override; ///< Run on layer stack attach
 		void onDetach() override; ///< Run on layer stack removal
 		void onUpdate(float deltaTime); ///< Run every frame
 		void onEvent(Events::Event& e) {}; ///< Run on an Event
 
-		void helloWorld();
+		void helloWorld(); ///< Test function to pass to a button click
 
-		void createButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, ImVec4 color, int corner, std::function<void()> func);
-		void createImageButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, int corner, const char* filepath);
-		void loadButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, ImVec4 color, int corner, std::function<void()> func);
-		void loadImageButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, int corner, unsigned int textureID);
+		void createButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, ImVec4 color, int corner, std::function<void()> func); ///< Takes multiple parameters and uses them to create an ImGUI button
+		void loadButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, ImVec4 color, int corner, std::function<void()> func); ///< Loads a button already in the buttonsToLoad vector to the screen
 
-		struct ImGuiImageButton {
-			ImGuiImageButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, int corner, unsigned int textureID)
-				: buttonName(buttonName), size(size), position(position), text(text), corner(corner), textureID(textureID) {
-			};
-
-			const char* buttonName;
-			std::pair<float, float> size;
-			std::pair<float, float> position;
-			const char* text;
-			int corner;
-			unsigned int textureID;
-
-		};
-
-		struct ImGuiButton {
-
+		struct ImGuiButton {  ///< Holds all the information about the ImGui button
 			ImGuiButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, ImVec4 color, int corner, std::function<void()> func)
 				: buttonName(buttonName), size(size), position(position), text(text), color(color), corner(corner), functionHolder(func) {
 			};
@@ -53,7 +38,6 @@ namespace Engine {
 			std::function<void()> functionHolder;
 		};
 
-		std::vector<ImGuiButton> buttonsToLoad;
-		std::vector<ImGuiImageButton> imageButtonsToLoad;
+		std::vector<ImGuiButton> buttonsToLoad; ///< Stores buttons that need to be rendered every frame to the screen
 	};
 }
--- sandbox/src/imguiLayer.cpp
+++ sandbox/src/imguiLayer.cpp
@@ -18,12 +18,9 @@ namespace Engine {
 
 		ImGui_ImplGlfwGL3_Init(static_cast<GLFWwindow*>(Application::getInstance().getWindow()->getNativeWindow()), true);
 
-
-
 		ImGuiIO& io = ImGui::GetIO();
 		ImFont* roboto = io.Fonts->AddFontFromFileTTF("../sandbox/assets/fonts/Roboto-Medium.ttf", 16.0f);
 
-		// Can call createButton from anywhere in the project and it will be added to the buttons to load each frame
 		createButton("2", { 200, 50 }, { 0, 0 }, "Top right corner button", ImVec4(0.8f, 0.4f, 0.5f, 1.0f), 1, [this] { helloWorld(); });
 	}
 
@@ -43,11 +40,7 @@ namespace Engine {
 				buttonsToLoad[i].text, buttonsToLoad[i].color, buttonsToLoad[i].corner, buttonsToLoad[i].functionHolder);
 		}
 
-		for (int i = 0; i < imageButtonsToLoad.size(); i++) {
-			loadImageButton(imageButtonsToLoad[i].buttonName, imageButtonsToLoad[i].size, imageButtonsToLoad[i].position, imageButtonsToLoad[i].text, imageButtonsToLoad[i].corner, imageButtonsToLoad[i].textureID);
-		}
-
-		ImGui::SetNextWindowBgAlpha(1.0f); // Make the ImGUI Debug backgrounds fully visible whilst allowing the button backgrounds to be hidden
+		ImGui::SetNextWindowBgAlpha(1.0f);
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 	}
@@ -67,7 +60,7 @@ namespace Engine {
 	{
 		ImGuiIO& io = ImGui::GetIO();
 
-		ImGui::SetNextWindowBgAlpha(0.0f); // Transparent background
+		ImGui::SetNextWindowBgAlpha(0.0f);
 
 		const float DISTANCE = 10.0f;
 		if (corner != -1)
@@ -97,48 +90,6 @@ namespace Engine {
 			}
 
 			ImGui::PopStyleColor(3);
-			ImGui::PopID();
-		}
-
-		ImGui::End();
-	}
-
-	void ImGUILayer::createImageButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, int corner, const char* filepath) {
-		texturePtr = std::shared_ptr<Renderer::Texture>(Renderer::Texture::createFromFile("../sandbox/assets/textures/buttonShiny.png"));
-		unsigned int texID = texturePtr->getId();
-
-		ImGuiImageButton imageButton(buttonName, size, position, text, corner, texID);
-		imageButtonsToLoad.push_back(imageButton);
-	}
-
-	void ImGUILayer::loadImageButton(const char* buttonName, std::pair<float, float> size, std::pair<float, float> position, const char* text, int corner, unsigned int textureID) {
-		ImGuiIO& io = ImGui::GetIO();
-
-		ImGui::SetNextWindowBgAlpha(0.0f); // Transparent background
-
-		const float DISTANCE = 10.0f;
-		if (corner != -1)
-		{
-			ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
-			ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
-			ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-		}
-		else {
-			ImGui::SetNextWindowPos({ position.first, position.second });
-		}
-
-		ImGuiStyle& style = ImGui::GetStyle();
-		style.WindowBorderSize = 0.0f;
-
-		float texWidth = (float)texturePtr->getWidth();
-		float texHeight = (float)texturePtr->getHeight();
-
-		if (ImGui::Begin(buttonName, false, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
-		{
-			ImGui::PushID(1);
-
-			ImGui::ImageButton((void*)textureID, ImVec2(size.first, size.second), { 0,0 }, { 1,1 }, -1);
-
 			ImGui::PopID();
 		}
 
